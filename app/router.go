616ee19cae8a5ae81a8b4b349965d88e3aa69a3f
Daniel <imamdnl11@gmail.com>
2022-07-02 11:37:58 +0700

    add router and main

--- app/router.go
+++ app/router.go
@@ -1,1 +1,21 @@
 package app
+
+import (
+	"booking-hotel/controller"
+	"booking-hotel/exception"
+	"github.com/julienschmidt/httprouter"
+)
+
+func NewRouter(discountController controller.DiscountController) *httprouter.Router {
+	router := httprouter.New()
+
+	router.GET("/api/discount", discountController.FindAllDiscount)
+	router.GET("/api/discount/:discountId", discountController.FindDiscountById)
+	router.POST("/api/discount", discountController.CreateDiscount)
+	router.PUT("/api/discount/:discountId", discountController.UpdateDiscount)
+	router.DELETE("/api/discount/:discountId", discountController.DeleteDiscount)
+
+	router.PanicHandler = exception.ErrorHandler
+
+	return router
+}
--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package booking_hotel
+
+import (
+	"booking-hotel/app"
+	"booking-hotel/controller"
+	"booking-hotel/helper"
+	"booking-hotel/middleware"
+	"booking-hotel/repository"
+	"booking-hotel/service"
+	"github.com/go-playground/validator/v10"
+	"net/http"
+)
+
+func main() {
+	db := app.NewDB()
+	validate := validator.New()
+	discountRepostiory := repository.NewDiscountRepository()
+	discountService := service.NesDiscountService(discountRepostiory, db, validate)
+	discountController := controller.NewDiscountController(discountService)
+
+	router := app.NewRouter(discountController)
+
+	server := http.Server{
+		Addr:    "localhost:3001",
+		Handler: middleware.NewAuthMiddleware(router),
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+}
